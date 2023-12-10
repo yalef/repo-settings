76b97de9328ee1c8db9b52e5632126f111974398
Vladislav <vladislav.romaschenko@saritasa.com>
2023-12-10 23:58:20 +0500

    Add schema in separate file

---
+++ src/api/schema.py
@@ -0,0 +1,49 @@
+import pydantic
+
+
+class Repository(pydantic.BaseModel):
+    name: str
+    full_name: str
+    id: int
+    node_id: str
+    private: bool
+
+
+class Installation(pydantic.BaseModel):
+    id: int
+
+
+class User(pydantic.BaseModel):
+    email: pydantic.EmailStr | None
+    name: str
+
+
+class Commit(pydantic.BaseModel):
+    added: list[str] | None
+    modified: list[str] | None
+    removed: list[str] | None
+    author: User
+    committer: User
+    distinct: bool
+    id: str
+    message: str
+    timestamp: str
+    tree_id: str
+    url: pydantic.AnyHttpUrl
+
+
+class PushWebhook(pydantic.BaseModel):
+    """Webhook for push event."""
+
+    after: str
+    before: str
+    base_ref: str | None
+    commits: list[Commit]
+    head_commit: Commit | None
+    compare: pydantic.AnyHttpUrl
+    created: bool
+    deleted: bool
+    forced: bool
+    ref: str
+    repository: Repository
+    installation: Installation
